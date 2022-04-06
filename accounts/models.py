4fb7b040d3088c41c7fb33aa0756b29b78240efb
hed0xakep <chugainov256@gmail.com>
2022-04-06 23:50:39 +0500

    функция для определения места в рейтинге

--- accounts/models.py
+++ accounts/models.py
@@ -32,10 +32,10 @@ class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=30)
     last_name =  models.CharField(max_length=30)
     email = models.EmailField()
-    gender = models.CharField(max_length=1, choices=GENDERS, default=' ')
+    gender = models.CharField(max_length=1, choices=GENDERS, default='m')
     info = models.CharField(max_length=700, blank=True)
     birth_date = models.DateField(default='2000-01-01')
-    public_stat = models.OneToOneField(PublicStatisticsModel, on_delete=models.CASCADE, null=True)
+    public_stat = models.OneToOneField(PublicStatisticsModel, on_delete=models.CASCADE, null=True, related_name='user')
     private_stat = models.OneToOneField(PrivateStatisticsModel, on_delete=models.CASCADE, null=True)
     avatar = models.ImageField(upload_to='avatars/', blank=True)
 
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -3,10 +3,22 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.http import Http404
 from matches.models import MatchModel
+from stats.models import PublicStatisticsModel
 from django.db.models import Q
 
 # Create your views here.
 CustomUser = get_user_model()
+
+def get_place(user, criterion):
+    stats_models = list(PublicStatisticsModel.objects.all().order_by(criterion))
+    place = 1
+    for model in stats_models:
+        print(model.user)
+        print(model.wins)
+        if model.user == user:
+            return place
+        else:
+            place +=1
 
 def showprofile(request, user):
 
@@ -14,6 +26,8 @@ def showprofile(request, user):
         raise Http404
     user = CustomUser.objects.get(username=user)
     matches = MatchModel.objects.filter(Q(p1=user) | Q(p2=user), is_confirmed=True)
+    win_place = get_place(user, 'wins')
+    print(win_place, 'place')
     context = {}
     context['user'] = user
     context['matches'] = matches
