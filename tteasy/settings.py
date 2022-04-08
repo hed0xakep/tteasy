0c49794a17b7699db03fa2fbb68f193a331f8dc5
hed0xakep <chugainov256@gmail.com>
2022-04-08 16:40:20 +0500

    подготовка к развертыванию

--- tteasy/settings.py
+++ tteasy/settings.py
@@ -133,7 +133,7 @@ MEDIA_DIR = os.path.abspath(BASE_DIR + MEDIA_URL)
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "**********************"
-EMAIL_HOST_PASSWORD = "*************"
+EMAIL_HOST_USER = "chugainov256@gmail.com"
+EMAIL_HOST_PASSWORD = "Arina22112005"
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
