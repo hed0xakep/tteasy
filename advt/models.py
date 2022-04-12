5733a3d9bec797046196ef5881c2e2cda624f7dc
hed0xakep <chugainov256@gmail.com>
2022-04-12 22:47:00 +0500

    добавлено 1 ajax-view

--- advt/models.py
+++ advt/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+'''
+import datetime
 
+date = datetime.datetime.strptime('2020-12-12', "%Y-%m-%d")
+res = date + datetime.timedelta(days=1)
+
+print(res.date()
+'''
 User = get_user_model()
-
+#пост для поиска напариников для игры
 class PostModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
-    date_pub = models.DateField(auto_now_add=True)
-    city = models.CharField(max_length=15)
-    delete_date = models.DateField(blank=True)
+    pub_date = models.DateField(auto_now_add=True)
+    city = models.CharField(max_length=2)#заменить на поле города!!
+    del_date = models.DateField(blank=True)
--- advt/views.py
+++ advt/views.py
@@ -1,20 +1,44 @@
 from django.contrib.auth import get_user_model
 from . import models, forms
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import datetime as dt
+
+'''
+my_string = "07.02.3000"
+my_dt = dt.datetime.strptime(my_string, '%d.%m.%Y')'''
 
 @login_required
 def add_post(request):
     if request.method == 'POST':
         form = forms.PostCreateForm(request.POST)
         if form.is_valid():
+            now = dt.datetime.now()
+            del_date = form.cleaned_data['del_date']
+            if dt.datetime.strptime(del_date, '%d.%m.%Y') < now:
+                response = {
+                    'success': False,
+                    'message': 'Неверная дата'
+                }
+                return JsonResponse(response)
+
             post = form.save(commit=False)
             post.user = request.user
+            post.del_date = dt.datetime.strptime(del_date, '%d.%m.%Y')
             post.save()
-            return HttpResponseRedirect(reverse('all_posts'))
-        return render(request, 'advt/add_post.html', {'form': form})
+            response = {
+                'sucess': True,
+                'message': 'Successfully created'
+                    }
+            return JsonResponse(reponse)
+        response = {
+                'errors': form.errors
+        }
+        return JsonResponse(response)
     form = forms.PostCreateForm
-    return render(request, 'advt/add_post.html', {'form': form})
+    now = dt.datetime.now()
+    return render(request, 'advt/add_post.html', {'form': form, 'now': now})
 
 def all_posts(request):
     if request.method == 'GET':
